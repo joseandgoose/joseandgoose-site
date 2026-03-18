3b1d3210a3c79ded149a73e125aa81cd8bfe27bc
Jose Delgado <josedelgado@Joses-MacBook-Air.local>
2026-03-17 22:37:19 -0700

    add sitemap and robots.txt

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.joseandgoose.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.joseandgoose.com/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.joseandgoose.com/work-and-projects',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.joseandgoose.com/writing',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.joseandgoose.com/contact',
+      lastModified: new Date(),
+    },
+  ]
+}
